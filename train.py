a1fbed29012a4048d6bfeea3eb1ba6a66b60687f
dionathomas <48287158+dionathomas@users.noreply.github.com>
2020-05-13 18:02:56 +0530

    smallervggnet

--- train.py
+++ train.py
@@ -1,106 +1,133 @@
-import numpy as np
+# set the matplotlib backend so figures can be saved in the background
 import matplotlib
-import matplotlib.pyplot as plt
-import os
-import glob
-import re
-import cv2
-from time import time
-from tensorflow.keras.callbacks import TensorBoard
-from sklearn.utils import shuffle
+matplotlib.use("Agg")
+# import the necessary packages
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator,img_to_array
-from tensorflow.keras import regularizers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
-from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
+from pyimagesearch.smallervggnet import SmallerVGGNet
+import matplotlib.pyplot as plt
+from imutils import paths
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
 
-def createModel(inputshape):
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=inputshape))
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
- 
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
- 
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset (i.e., directory of images)")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output model")
+ap.add_argument("-l", "--labelbin", required=True,
+	help="path to output label binarizer")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output accuracy/loss plot")
+args = vars(ap.parse_args())
 
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+# initialize the number of epochs to train for, initial learning rate,
+# batch size, and image dimensions
+EPOCHS = 75
+INIT_LR = 1e-3
+BS = 32
+IMAGE_DIMS = (96, 96, 3)
 
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(12, activation='softmax'))
-     
-    return model
+# grab the image paths and randomly shuffle them
+print("[INFO] loading images...")
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+# initialize the data and labels
+data = []
+labels = []
 
-def natural_key(string_):
-    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+# loop over the input images
+for imagePath in imagePaths:
+	# load the image, pre-process it, and store it in the data list
+	image = cv2.imread(imagePath)
+	image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
+	image = img_to_array(image)
+	data.append(image)
+	# extract set of class labels from the image path and update the
+	# labels list
+	l = label = imagePath.split(os.path.sep)[-2].split("_")
+	labels.append(l)
 
-paths = []
-for i in range(1,13):
-    paths.append('/WoodDataset/Train/' + str(i) + "/")
+	# scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+print("[INFO] data matrix: {} images ({:.2f}MB)".format(
+	len(imagePaths), data.nbytes / (1024 * 1000.0)))
 
-uri = []
-for i in paths:
-    uri.append(sorted(glob.glob(os.path.join(os.getcwd() + i, '*.jpg')), key=natural_key))
+# binarize the labels using scikit-learn's special multi-label
+# binarizer implementation
+print("[INFO] class labels:")
+mlb = MultiLabelBinarizer()
+labels = mlb.fit_transform(labels)
+# loop over each of the possible class labels and show them
+for (i, label) in enumerate(mlb.classes_):
+	print("{}. {}".format(i + 1, label))
 
-width = 200
-height = 100
-shape = (height,width,3)
-dataset = []
-label = []
+# partition the data into training and testing splits using 80% of
+# the data for training and the remaining 20% for testing
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.2, random_state=42)
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
+							 height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+							 horizontal_flip=True, fill_mode="nearest")
 
-for u in uri:
-    i = 0 
-    for path in u:
-        img = cv2.imread(path)
-        img = cv2.resize(img,(width,height))
-        img = img_to_array(img)
-        dataset.append(img)
-        temp = [0,0,0,0,0,0,0,0,0,0,0,0]
-        temp[i] = 1
-        label.append(temp)
-        i += 1
+# initialize the model using a sigmoid activation as the final layer
+# in the network so we can perform multi-label classification
+print("[INFO] compiling model...")
+model = SmallerVGGNet.build(
+	width=IMAGE_DIMS[1], height=IMAGE_DIMS[0],
+	depth=IMAGE_DIMS[2], classes=len(mlb.classes_),
+	finalAct="sigmoid")
+# initialize the optimizer
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 
-dataset = np.array(dataset, dtype="float") / 255.0
-label = np.array(label, dtype = 'int')
-dataset,label = shuffle(dataset,label)
+# compile the model using binary cross-entropy rather than
+# categorical cross-entropy -- this may seem counterintuitive for
+# multi-label classification, but keep in mind that the goal here
+# is to treat each output label as an independent Bernoulli
+# distribution
+model.compile(loss="binary_crossentropy", optimizer=opt,
+				  metrics=["accuracy"])
+# train the network
+print("[INFO] training network...")
+H = model.fit_generator(
+	aug.flow(trainX, trainY, batch_size=BS),
+		validation_data=(testX, testY),
+		steps_per_epoch=len(trainX) // BS,
+		epochs=EPOCHS, verbose=1)
 
-(trainX, testX, trainY, testY) = train_test_split(dataset,label)
+# save the model to disk
+print("[INFO] serializing network...")
+model.save(args["model"])
+# save the multi-label binarizer to disk
+print("[INFO] serializing label binarizer...")
+f = open(args["labelbin"], "wb")
+f.write(pickle.dumps(mlb))
+f.close()
 
-model = createModel(shape)
-model.compile(optimizer = tf.keras.optimizers.SGD(lr = 0.03,momentum = 0.1), 
-    loss=tf.keras.losses.categorical_crossentropy,
-    metrics=[tf.keras.metrics.categorical_accuracy,tf.keras.metrics.mae]
-)
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+N = EPOCHS
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="upper left")
+plt.savefig(args["plot"])
 
-tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
 
-cb = [tensorboard,tf.keras.callbacks.ModelCheckpoint('./savedmodel/model{epoch:04d}-{val_categorical_accuracy:.4f}.h5', monitor='val_categorical_accuracy', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)]
 
-history = model.fit(trainX,trainY,epochs = 200,batch_size = 9,validation_data=(testX,testY),callbacks = cb)
-test_loss, test_acc, test_mae = model.evaluate(dataset, label)
-print('Test accuracy:', test_acc)
-print('Test loss:', test_loss)
 
-fig = plt.figure(figsize=[8,6])
-
-ax1 = fig.add_subplot(121)
-ax1.plot(history.history['loss'],'r',linewidth=3.0)
-ax1.plot(history.history['val_loss'],'b',linewidth=3.0)
-ax1.legend(['Training loss', 'Validation Loss'],loc = 'lower right',fontsize=8)
-ax1.set_xlabel('Epochs ',fontsize=16)
-ax1.set_ylabel('Loss',fontsize=16)
-ax1.set_title('Loss Curves',fontsize=16)
